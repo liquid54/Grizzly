bc21544613d68a353a607f42e1f265b75a22d1c8
liquid54 <dk.sudent.course@gmail.com>
2025-03-13 17:53:50 +0200

    refactor components and styles; update layout, improve and enhance responsiveness

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -6,9 +6,9 @@ import Link from "next/link";
 
 const SignUp = () => {
     return (
-        <div className='py-[52px] px-[710px]'>
-            <div className='flex flex-col gap-y-[20px] items-center'>
-                <div className='flex flex-col gap-y-[10px] items-center justify-center pb-[30px]'>
+        <div className='py-[52px] px-[515px]'>
+            <div className='flex flex-col gap-y-[30px] items-center'>
+                <div className='flex flex-col gap-y-[30px] items-center'>
                     <Image
                         width={1920}
                         height={1080}
@@ -16,26 +16,25 @@ const SignUp = () => {
                         alt="logo"
                         className="rounded-full w-[100px] h-[100px]"
                     />
-                    <ThemedText type='title' className='text-[36px]'>Sign Up</ThemedText>
-                    <div className='flex flex-row gap-x-[10px] items-center'>
-                        <ThemedText className='text-[#8A8B8E]'>Already have an account?</ThemedText>
-                        <Link href='/login'><ThemedText type='link_text'>Log In</ThemedText></Link>
-                    </div>
+                </div>
+                <ThemedText type='title' className='text-[36px]'>Sign Up</ThemedText>
+                <div className='flex flex-row gap-x-[10px] items-center'>
+                    <ThemedText className='text-[#8A8B8E]'>Already have an account?</ThemedText>
+                    <Link href='/login'><ThemedText type='link_text'>Log In</ThemedText></Link>
                 </div>
 
                 <Input label={'Email*'} placeholder={'Write your email'} className="w-full"/>
                 <div className='flex flex-col gap-y-[6px] w-full'>
-                    <Input label={'Password*'} placeholder={'Write your password'} className="w-full"/>
+                    <Input label={'Password*'} placeholder={'Write your code'} className="w-full"/>
                     <ThemedText type='link_text'>Show password</ThemedText>
                 </div>
                 <div className='flex flex-col gap-y-[6px] w-full'>
-                    <Input label={'Repeat Password*'} placeholder={'Repeat your password'} className="w-full"/>
+                    <Input label={'Repeat Password*'} placeholder={'Write your code'} className="w-full"/>
                     <ThemedText type='link_text'>Show password</ThemedText>
                 </div>
                 <Input label={'Telegram'} placeholder={'Write your Telegram number'} className="w-full"/>
                 <Input label={'Whatsapp'} placeholder={'Write your Whatsapp number'} className="w-full"/>
-
-                <Link href='/login' className='pt-[10px]'><Button size='mid_large'>Sign Up</Button></Link>
+                <Link href='/login'><Button>Sign Up</Button></Link>
             </div>
         </div>
     );
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,50 +1,61 @@
-    import {ThemedText} from "@/components/ThemedText";
-    import Button from "@/components/Button";
-    import Image from "next/image";
-    import Exchange from "@/components/Exchange";
-    import ExchangeList from "@/components/ExchangeList";
-    import AboutUs from "@/components/AboutUs";
-    import Link from "next/link";
+import {ThemedText} from "@/components/ThemedText";
+import Button from "@/components/Button";
+import Image from "next/image";
+import Exchange from "@/components/Exchange";
+import ExchangeList from "@/components/ExchangeList";
+import AboutUs from "@/components/AboutUs";
+import Link from "next/link";
+
+import BackgroundCircles from "@/components/Main/bgCircles/BgCircles";
 
 
-    const HomePage = () => {
-        return (
-            <>
-                <div className='px-[250px] pt-[158px]'>
-                    <div className='flex flex-row justify-between space-x-[189px]'>
-                        <div className='flex flex-col space-y-[16px]'>
-                            <div className='flex flex-col'>
-                                <ThemedText type='heading_blue'>Buy and Sell</ThemedText>
-                                <ThemedText type='heading'>Cryptocurrency Instantly</ThemedText>
+const HomePage = () => {
+    return (
+        <>
+            <div className=''>
+
+                <div className="relative w-full overflow-hidden">
+                    {/* SVG Background Circles */}
+                    <BackgroundCircles/>
+                    {/* Основний контент */}
+                    <div
+                        className="flex flex-row justify-between space-x-[189px] relative pt-[158px] pb-[272px] z-10 px-[250px]">
+                        <div className="flex flex-col space-y-[16px] z-10 justify-center">
+                            <div className="flex flex-col space-y-[16px]">
+                                <ThemedText type="heading_blue">Buy and Sell</ThemedText>
+                                <ThemedText type="heading">Cryptocurrency</ThemedText>
+                                <ThemedText type="heading">Instantly</ThemedText>
                             </div>
-                            <ThemedText>Lorem ipsum dolor sit amet consectetur. Pellentesque est cursus pretium ut egestas.
-                                Viverra pellentesque elementum tristique nibh urna bibendum sollicitudin elit
-                                bibendum.
+                            <ThemedText>
+                                Lorem ipsum dolor sit amet consectetur. Pellentesque est cursus pretium ut egestas.
+                                Viverra pellentesque elementum tristique nibh urna bibendum sollicitudin elit bibendum.
                             </ThemedText>
-                            <Link href='/exchange'>
-                                <Button variant='primary' size='medium'>Exchange</Button>
+                            <Link href="/exchange">
+                                <Button variant="primary" size="medium">Exchange</Button>
                             </Link>
                         </div>
                         <Image
                             width={1920}
                             height={1080}
                             src="/IMG_5815 (1).png"
-                            alt="logo"
-                            className="flex rounded-full w-[566px] h-[566px]"
+                            alt="Ведмідь в худі Balenciaga"
+                            className="object-cover w-[566px] h-[566px] rounded-full"
                         />
                     </div>
-                    <div className='pt-[370px] flex flex-row gap-x-[53px]'>
-                        <Exchange fullWidthButton={true} visibleBorder={true}/>
-                        <div className='flex  justify-center flex-col '>
-                            <ThemedText type='title'>Cryptocurrency</ThemedText>
-                            <ThemedText type='title'>Exchange</ThemedText>
-                        </div>
-                    </div>
                 </div>
-                <ExchangeList/>
-                <AboutUs/>
-            </>
-        );
-    }
 
-    export default HomePage;
+
+                <div className='pt-[133px] flex gap-x-[53px] justify-center items-center'>
+                    <Exchange fullWidthButton={true} visibleBorder={true}/>
+                    <ThemedText type='title' className='text-wrap'>Cryptocurrency Exchange</ThemedText>
+                </div>
+            </div>
+            <ExchangeList/>
+            <div className='px-[370px] py-[150px]'>
+                <AboutUs/>
+            </div>
+        </>
+    );
+}
+
+export default HomePage;

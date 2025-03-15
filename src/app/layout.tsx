bd5551105311dbf7a4b11b1c449356392842dfab
naytin <worlddesign1987@gmail.com>
2025-03-15 13:30:00 +0200

    fix fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,4 @@
-import localFont from 'next/font/local';
-import { Poppins } from 'next/font/google';
 import './globals.css';
-
-export const poppins = Poppins({
-    subsets: ['latin'],
-    weight: ['400', '500', '600'],
-    variable: '--font-poppins',
-});
-
-export const mont = localFont({
-    src: [
-        {
-            path: '../../public/fonts/Mont-SemiBold.ttf',
-            weight: '600',
-            style: 'normal',
-        },
-        {
-            path: '../../public/fonts/Mont-Regular.ttf',
-            weight: '400',
-            style: 'normal',
-        },
-    ],
-    variable: '--font-mont',
-});
 
 export default function RootLayout({
     children,
@@ -30,7 +6,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }) {
     return (
-        <html lang='en' className={mont.className}>
+        <html lang='en'>
             <body>{children}</body>
         </html>
     );

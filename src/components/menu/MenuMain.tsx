052c72042b1ee7be8d010ddc316e08c99a7e85c6
liquid54 <dk.sudent.course@gmail.com>
2025-03-16 14:19:25 +0200

    backed updates

--- src/components/menu/MenuMain.tsx
+++ src/components/menu/MenuMain.tsx
@@ -1,10 +1,8 @@
 import {OpenMenuProps} from "@/types/main";
 import {usePathname} from "next/navigation";
 import MenuWrapper from "@/components/menu/MenuWrapper";
-import {MAIN_MENU_LINKS} from "@/constants/panel-links";
+import {USER_MENU_LINKS} from "@/constants/panel-links";
 import SettingsMenuButton from "@/features/user-dashboard/components/settings-menu/SettingMenuButton";
-import Button from "@/components/Button";
-import LanguageSwitcher from "@/components/User/LanguageSelector";
 
 const SettingsMenu = ({isOpen, setIsOpen}: OpenMenuProps) => {
     const pathname = usePathname();
@@ -14,7 +12,7 @@ const SettingsMenu = ({isOpen, setIsOpen}: OpenMenuProps) => {
         <MenuWrapper isOpen={isOpen} setIsOpen={setIsOpen}>
             <nav className='flex flex-col justify-between h-full pt-8 lg:pt-0'>
                 <div className="flex flex-col gap-5">
-                    {MAIN_MENU_LINKS.map((l, i) => {
+                    {USER_MENU_LINKS.map((l, i) => {
                         const current = l.href?.replace('/main/', '') || '';
                         return (
                             <SettingsMenuButton
@@ -28,18 +26,6 @@ const SettingsMenu = ({isOpen, setIsOpen}: OpenMenuProps) => {
                     })}
                 </div>
 
-                <div className="flex flex-col">
-                    <LanguageSwitcher/>
-                    <div className='space-x-4'>
-                        <Button variant="primary" size="tiny" className='md:w-[256px] sm:w-[171px]'>
-                            NewB
-                        </Button>
-
-                        <Button variant="secondary" size="tiny" className='sm:w-[256px] sm:w-[171px]'>
-                            NewA
-                        </Button>
-                    </div>
-                </div>
             </nav>
         </MenuWrapper>
     );

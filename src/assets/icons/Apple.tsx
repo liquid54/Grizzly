231ce21f10340c216e2edbdd2639ec9c3875fc14
naytin <worlddesign1987@gmail.com>
2025-03-19 17:26:51 +0200

    FIX | replce Apple.png to apple.png

--- src/assets/icons/Apple.tsx
+++ src/assets/icons/Apple.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 const Apple = () => {
     return (
         <div className='relative w-[30px] h-[30px] sm:w-[36px] sm:h-[36px]'>
-            <Image src='/Apple.png' alt='Apple' width={814} height={1000} />
+            <Image src='/apple.png' alt='Apple' width={814} height={1000} />
         </div>
     );
 };
